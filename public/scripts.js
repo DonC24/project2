fa56e3ddc9df86ecd776e890c984b52bc8da6526
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-07-31 22:38:37 +0800

    Added basic notifiction. does not use variable timeout. Testing actions.

--- public/scripts.js
+++ public/scripts.js
@@ -8,13 +8,55 @@ window.onload = () => {
       console.log(result);
     });
 
+
+
+
+
+
+    let spawnNotification = function(Notice,theBody,shouldRequireInteraction) {
+        var options = {
+            body: "This is a notification",
+            requireInteraction: true
+        }
+        var n = new Notification(Notice, options);
+    }
+
+    setTimeout(spawnNotification, 2000);
+
+
+
+
+    self.registration.showNotification("Take your medicine!", {
+        actions: [
+            {
+                action: 'Confirm taken',
+                title: 'Confirm'
+            },
+            {
+                action: 'Snooze',
+                title: 'Snooze'
+            }
+        ]
+    });
+
+    self.addEventListener('notificationclick', function(event) {
+        event.notification.close();
+        if (event.action === 'archive') {
+            // Archive action was clicked
+            archiveEmail();
+        } else {
+            // Main body of notification was clicked
+            clients.openWindow('/inbox');
+        }
+    }, false);
+
     //const myVar; // 1 day in milliseconds
-    const notificationTimer = localStorage.getItem('notificationTimer');
+    /*const notificationTimer = localStorage.getItem('notificationTimer');
     if(notificationTimer){
         const sendNotification = (Date.now() - (new Date(notificationTimer)).getTime()) > myVar;
         if(sendNotification){
             const notification = new Notification('Displaying reminder to take your meds');
         }
-    }
+    }*/
 
 };
