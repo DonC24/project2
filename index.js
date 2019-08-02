ad5652bb635e3bf5e3399954f2589f4ebebb6521
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-08-02 12:16:21 +0800

    Heroku changes - db settings

--- index.js
+++ index.js
@@ -13,15 +13,44 @@ let sha256 = require('js-sha256');
 const SALT = "ALL YOUR BASE ARE BELONG TO US";
 
 // Initialise postgres client
-const configs = {
-    user: 'donc',
-    host: '127.0.0.1',
-    database: 'medtrack_db',
-    port: 5432,
-    password: 'password'
-};
 
+//require the url library
+//this comes with node, so no need to yarn add
+const url = require('url');
+
+//check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  var configs = {
+        user: 'donc',
+        host: '127.0.0.1',
+        database: 'medtrack_db',
+        port: 5432,
+        password: 'password'
+  };
+}
+
+//this is the same
 const pool = new pg.Pool(configs);
+
 
 pool.on('error', function (err) {
   console.log('idle client error', err.message, err.stack);
@@ -333,6 +362,23 @@ app.put('/meds/:id', (request, response) => {
 });*/
 
 
+app.post('meds/:id/confirm', (request, response) => {
+    console.log(request.body);
+    console.log("id " + request.params.id);
+    let timestamp = Date.now();
+    const queryString = "INSERT INTO confirmation (medication_id, time_taken) VALUES ($1, $2) RETURNING *"
+    const values = [request.body, timestamp];
+
+    pool.query(queryString, values, (err, res) => {
+        if (err) {
+            console.log("query error", err.message);
+        } else {
+            console.log("confirmation inserted");
+        }
+    })
+})
+
+
 //POST register user
 app.post('/users', (request, response) => {
     // hash the password
@@ -419,7 +465,9 @@ app.get('/', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
 
--- public/scripts.js
+++ public/scripts.js
@@ -19,6 +19,7 @@ let confirmEat = function(){
             nextHeader.appendChild(confirmBtn);
 
             confirmBtn.addEventListener('click', doRequest);
+            confirmBtn.addEventListener('click', confirmReq);
 
     //}
 
@@ -43,6 +44,30 @@ var doRequest = function(){
 
     // ready the system by calling open, and specifying the url
     request.open("POST", "http://localhost:3000/meds/" + userId + "?_method=PUT");
+
+    // send the request
+    request.send();
+};
+
+var confirmReq = function(){
+
+    console.log("userId in confirm request " + userId);
+
+    // what to do when we recieve the request
+    var responseHandler = function() {
+        console.log("response text", this.responseText);
+        console.log("status text", this.statusText);
+        console.log("status code", this.status);
+    };
+
+    // make a new request
+    var request = new XMLHttpRequest();
+
+    // listen for the request response
+    request.addEventListener("load", responseHandler);
+
+    // ready the system by calling open, and specifying the url
+    request.open("POST", "http://localhost:3000/meds/" + userId + "/confirm");
 
     // send the request
     request.send();
